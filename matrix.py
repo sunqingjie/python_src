a4b45d4ed7220e7245fd770de5917cf583c4f3cb
northland <northland@sina.com>
2018-06-13 20:30:32 +0800

    my first file on github

---
+++ matrix.py
@@ -0,0 +1,47 @@
+
+# coding:gbk 
+  
+def matrix(n):
+    
+    block = n - 1  # there are four block in one circle
+
+    result = dict()
+    
+    row = 0
+    col = 0
+    
+    number = 1
+    while block >= 0:
+        row = row + 1
+        col = col + 1
+        
+        if block == 0 : result[(row,col)] = number # number in the center
+            
+        for direction in [(0,1),(1,0),(0,-1),(-1,0)]:  # [north,east,south,west], clockwise
+            drow,dcol = direction
+            for i in range(1,block + 1):
+                result[(row,col)] = number
+                row = row + drow
+                col = col + dcol
+                number = number + 1
+    
+    
+        
+        block = block - 2 # next block length
+    
+    return result        
+    
+
+if __name__ == '__main__':  # test
+    
+    print "git test"    
+    
+    n = 5
+    result = matrix(n)
+    
+    for i in range(1,n+1):
+        for j in range(1,n+1):        
+            print str(result[(i,j)]).rjust( len(str(n*n))),
+        print "\n"
+        
+print "≤‚ ‘"
